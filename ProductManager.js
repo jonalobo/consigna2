4231ceb20e28fd9171b75e5348c69a8e9232258e
jonalobo <jonathan_1920@hotmail.com>
2023-04-09 00:05:43 -0600

    Listo

---
+++ ProductManager.js
@@ -0,0 +1,27 @@
+let productos = []
+class ProductManager {
+    constructor(title,description,price,thumbnail,code,stock){
+        this.id = productos.length,
+        this.title = title,
+        this.description = description,
+        this.price = price,
+        this.thumbnail = thumbnail,
+        this.code = code,
+        this.stock = stock
+    }
+    addProduct(){
+
+    }
+    getProducts(){
+
+    }
+    getProductById(){
+
+    }
+    updateProduct(){
+
+    }
+    deleteProduct(){
+
+    }
+}
