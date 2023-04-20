2dea221d4a32f625ebbbda753b78a74c8bb21a9f
jonalobo <jonathan_1920@hotmail.com>
2023-04-19 20:02:01 -0600

    Falta update y delete

--- ProductManager.js
+++ ProductManager.js
@@ -1,3 +1,5 @@
+const fs = require('fs')
+
 let productos = []
 class ProductManager {
     constructor(title,description,price,thumbnail,code,stock){
@@ -10,13 +12,32 @@ class ProductManager {
         this.stock = stock
     }
     addProduct(){
-
+        let id = productos.length + 1
+        const producto = {
+            id,
+            title : this.title,
+            description: this.description,
+            price: this.price,
+            thumbnail: this.thumbnail,
+            code: this.code,
+            stock: this.stock
+        }
+        productos.push(producto)
+        const data = JSON.stringify(productos)
+        guardarProductos('productos.txt',data)
     }
-    getProducts(){
-
+   async getProducts(){
+        const productosSinConvertir = await leerProductos()
+        console.log(JSON.parse(productosSinConvertir))
     }
-    getProductById(){
-
+   async  getProductById(id){
+        const productosSinParsear = await leerProductos()
+        const demo = JSON.parse(productosSinParsear)
+        demo.map((e)=>{
+            if (e.id == id) {
+                console.log(e)
+            }
+        })
     }
     updateProduct(){
 
@@ -25,3 +46,17 @@ class ProductManager {
 
     }
 }
+
+
+async function guardarProductos(nombre,data) {
+    await fs.promises.writeFile(nombre,data)
+}
+async function leerProductos() {
+    let resultado = await fs.promises.readFile('productos.txt','utf-8')
+    return resultado
+}
+const producto1 = new ProductManager('laptop','demo','demo','demo','demo','demo','demo')
+const producto2 = new ProductManager('laptop','demo','demo','demo','demo','demo','demo')
+producto1.addProduct()
+producto2.addProduct()
+producto2.getProducts()
